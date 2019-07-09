23b08ecc3daa00f732d150d19042430d3f72dfe3
ArsenyZhivitsky <36786913+ArsenyZhivitsky@users.noreply.github.com>
2019-07-09 15:22:22 +0300

    adding views

---
+++ FeedbackWebsite/FeedbackWebsite/Models/AnswersInfoModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using FeedbackWebsite.Utils;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace FeedbackWebsite.Models
+{
+    public enum Answer
+    {
+        [Display(Name = "Strongly Disagree")]
+        StronglyDisagree,
+        [Display(Name = "Disagree")]
+        Disagree,
+        [Display(Name = "Neither Agree nor Disagree")]
+        NeitherAgreeNorDisagree,
+        [Display(Name = "Agree")]
+        Agree,
+        [Display(Name = "Strongly Agree")]
+        StronglyAgree
+    }
+
+    public class AnswersInfoModel
+    {
+        public int Id { get; set; }
+
+        public int PersonId { get; set; }
+
+        public int QuestionId { get; set; }
+    }
+
+    public class AnswerEnum : AnswersInfoModel
+    {
+        public Answer QuestionEnumAnswer { get; set; }
+    }
+
+    public class AnswerText : AnswersInfoModel
+    {
+        public string AnswerTextAnswer { get; set; }
+    }
+}
