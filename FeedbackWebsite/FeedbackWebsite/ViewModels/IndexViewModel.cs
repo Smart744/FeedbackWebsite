1d6fa6636cd4599676d73a95fc2431ed7b08a170
ArsenyZhivitsky <36786913+ArsenyZhivitsky@users.noreply.github.com>
2019-07-09 15:23:05 +0300

    adding ViewModels

---
+++ FeedbackWebsite/FeedbackWebsite/ViewModels/IndexViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FeedbackWebsite.Models;
+
+namespace FeedbackWebsite.ViewModels
+{
+    public class IndexViewModel
+    {
+        //person id
+        public int personId { get; set; }
+        //list
+        //   qid  q t a 
+
+        public IEnumerable<QuestionsAnswersModel> qiestionsAnswers { get; set; }
+        //public IEnumerable<QuestionTextModel> QuestionsText { get; set; }
+    }
+}
---
+++ FeedbackWebsite/FeedbackWebsite/ViewModels/QuestionsAnswersModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FeedbackWebsite.Models;
+
+namespace FeedbackWebsite.ViewModels
+{
+    public class QuestionsAnswersModel
+    {
+        public int QuestionId { get; set; }
+        
+        public string QuestionText { get; set; }
+
+        public bool QuestionType { get; set; }
+    }
+
+    public class QuestionsAnswersEnum : QuestionsAnswersModel
+    {
+        public Answer EnumAnswer { get; set; }
+    }
+
+    public class QuestionsAnswersText : QuestionsAnswersModel
+    {
+        public string TextAnswer { get; set; }
+    }
+}
