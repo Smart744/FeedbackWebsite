348d36fa050625aa8b059d997b2002f642102499
ArsenyZhivitsky <36786913+ArsenyZhivitsky@users.noreply.github.com>
2019-07-09 15:24:02 +0300

    adding utills

---
+++ FeedbackWebsite/FeedbackWebsite/Utils/EnumExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+
+namespace FeedbackWebsite.Utils
+{
+    public static class EnumExtensions
+    {
+        private static readonly ConcurrentDictionary<string, string> DisplayNameCache = new ConcurrentDictionary<string, string>();
+
+        public static string DisplayName(this Enum value)
+        {
+            var key = $"{value.GetType().FullName}.{value}";
+
+            var displayName = DisplayNameCache.GetOrAdd(key, x =>
+            {
+                var name = (DisplayNameAttribute[]) value
+                    .GetType()
+                    .GetTypeInfo()
+                    .GetField(value.ToString())
+                    .GetCustomAttributes(typeof(DisplayNameAttribute), false);
+
+                return name.Length > 0 ? name[0].DisplayName : value.ToString();
+            });
+
+            return displayName;
+        }
+    }
+}
